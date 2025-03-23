97fe445508b8b23ce314b25ca02a73c87cce5868
kakashihatakae <shreyasb2@gmail.com>
2025-03-23 15:27:41 -0700

    feat: add analytics to the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import clsx from "clsx";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
           )}
         >
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
